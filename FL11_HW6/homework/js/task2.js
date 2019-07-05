1ae414cdb4c703aa95c00d2f0e46f85c43c56d8d
norka2233 <norka2233@gmail.com>
2019-07-05 12:13:07 +0300

    2 task

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,18 @@
-// Your code goes here
+const a = prompt('Enter length of side A');
+const b = prompt('Enter length of side B');
+const c = prompt('Enter length of side C');
+
+if (c < a + b & a < c + b & b < c + a) {
+  if (a === b & b === c & a === c) {
+    console.log('Equivalent triangle')
+  }
+  else if (a === b || b === c || a === c) {
+    console.log('Isosceles triangle')
+  }
+  else {
+    console.log('Normal triangle');
+  }
+}
+else {
+  console.log('Triangle doesn’t exist');
+}
