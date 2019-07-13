02f875e77a766576f8151cebef9a691267bd3d6c
norka2233 <norka2233@gmail.com>
2019-07-13 18:11:01 +0300

    format

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -1,8 +1,3 @@
-formatTime(120); //=> 0 day(s) 2 hour(s) 0 minute(s).
-formatTime(59); //=> 0 day(s) 0 hour(s) 59 minute(s).
-formatTime(3601); //=> 1 day(s) 0 hour(s) 1 minute(s).
-
-//1890
 // day = 24 hours => 24 * 60 = 1440; - 1 day
 // hour = 60 min
 // minute = 1 min
@@ -16,4 +11,4 @@ function formatTime(number) {
   return (day + 'day(s) ' + hour + 'hour(s) ' + minute + 'minute(s) ');
 }
 
-formatTime(12);
+formatTime(1220);
