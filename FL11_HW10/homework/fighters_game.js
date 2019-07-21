1ba0d0de112b707ab39d5e760b24b31c2db93622
norka2233 <norka2233@gmail.com>
2019-07-21 22:07:22 +0300

    first function all

--- FL11_HW10/homework/fighters_game.js
+++ FL11_HW10/homework/fighters_game.js
@@ -1,8 +1,59 @@
-class Fighterew Fighter{
-    constructor() {
-      name: ‘John’,
-      damage: 20,
-      hp: 100, 
-      agility: 25
-    };
+class Fighter{
+    constructor(obj) {
+     this.name = obj.name;
+     this.damage = obj.damage;
+     this.hp = obj.hp;
+     this.agility = obj.agility;
+     this.win = 0;
+     this.losses = 0;
+     this.totalHp = obj.hp;
+    }
+    getName() {
+        return this.name;
+    }
+    getDamage() {
+        return this.damage;
+    }
+    getAgility() {
+        return this.agility;
+    }
+    getHealth() {
+        return this.hp;
+    }
+    dealDamage(damage) {
+        if(this.hp - damage < 0) {
+            this.hp = 0;
+        } else {
+            this.hp = damage;
+        }
+    }
+    attack(fighter) {
+        let k = 100;
+        let probability = Math.random() * k;
+        if(probability > fighter.getAgility) {
+            fighter.dealDamage(this.damage);
+            console.log(`${this.getName} make ${this.getDamage} to ${fighter.getName}`);
+            
+        } else {
+            console.log(`${fighter.getName} attack missed`);
+        }
+    }
+    logCombatHistory() {
+        console.log(`Name: ${this.getName}, Wins: ${this.win}, Losses: ${this.losses}`);
+    }
+    heal(hpAmount) {
+        if (this.hp + hpAmount >= this.totalHp) {
+            return this.getHealth;
+        }
+        else {
+            this.hp += hpAmount;
+        }
+    }
+    addWin() {
+        this.win++;
+    }
+    addLoss() {
+        this.losses++;
+    }
+}
 
